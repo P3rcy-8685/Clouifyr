1c5a34ea5aaa0fb4a6291dd73c2850f0a313c84f
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2023-03-26 02:19:02 +0530

    loading

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -8,7 +8,7 @@ const Upload = () => {
   const [charge, setCharge] = useState("");
   const [desc, setDesc] = useState("");
   const [file, setFile] = useState("");
-
+  const [loading, setLoading] = useState(false);
   const progressCallback = (progressData) => {
     let percentageDone =
       100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
@@ -16,6 +16,7 @@ const Upload = () => {
   };
 
   const deploy = async (title, file, desc, charge) => {
+    setLoading(true);
     const output = await lighthouse.upload(
       file,
       "f3a509cf-00d5-4c5c-ad49-c9e033a8805f",
@@ -48,9 +49,14 @@ const Upload = () => {
         console.log("added on chain tx:", tx);
       });
     const response = await axios.post("http://20.197.4.167:3000/deploy", data);
+    console.log(response);
+    setLoading(false);
   };
 
   return (
+    // if loading is false render the code
+    // else render the loading screen
+    loading ? (
     <div className="appInput">
       <div className="appForm">
         <h1>
@@ -110,8 +116,16 @@ const Upload = () => {
           Upload
         </button>
       </div>
-    </div>
+    </div>) : (
+      <div className="loading">
+        <div className="loadingText">
+          <h1>Deploying App</h1>
+          <h2>Please wait...</h2>
+        </div>
+      </div>
+    )
   );
 };
+
 
 export default Upload;
